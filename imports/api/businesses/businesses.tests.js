1efc4ae6f8c01701f0fc0953790d43952effbc18
Conner Davis <connergdavis@gmail.com>
2018-10-26 00:51:27 -0700

    Fixed React test on Businesses

--- imports/api/businesses/businesses.tests.js
+++ imports/api/businesses/businesses.tests.js
@@ -1,53 +1,67 @@
 import Adapter from 'enzyme-adapter-react-16';
-import { assert } from 'chai';
+import chai from 'chai';
 import Enzyme from 'enzyme';
 import faker from 'faker';
 import React from 'react';
-import { resetDatabase } from 'meteor/xolvio:cleaner';
-import { shallow } from 'enzyme';
+import { Card, CardImg, CardBody, CardTitle, CardText, CardFooter, Button } from 'reactstrap';
+import { Factory } from 'meteor/dburles:factory';
 
-import { Businesses } from './businesses';
+import Businesses from './businesses';
 import Business from '/imports/ui/components/Business';
-
-// macro to automatically reset test database to empty
-Meteor.methods({
-  'test.resetDatabase': () => resetDatabase(),
-});
 
 // configure React test adapter
 Enzyme.configure({ adapter: new Adapter() });
 
-// generate fake test data
-Factory.define('business', Businesses, {
-  name: () => faker.lorem.word(),
-  desc: () => faker.lorem.sentence(),
+Meteor.methods({
+  /* clear stub data */
+  'test.restoreDatabase': () => StubCollections.restore(),
+  
+  /* generate stub data */
+  'test.stubDatabase': () => StubCollections.stub(['businesses']),
 });
+
+Factory.define('business', Businesses, {
+  name: () => faker.company.companyName(),
+  desc: () => faker.company.catchPhrase(),
+  photo: () => faker.image.imageUrl(),
+  country: () => faker.address.country(),
+  streetAddress: () => faker.address.streetAddress(),
+  state: () => faker.address.state(),
+  city: () => faker.address.city(),
+  zip: () => faker.address.zipCode(undefined),
+  phoneNumber: () => faker.phone.phoneNumber(),
+  website: () => faker.internet.url(),
+  type: () => faker.commerce.product(),
+  verified: false,
+});
+
+let expect = chai.expect;
 
 describe('Businesses', () => {
 
-  const biz = Factory.create('business');
-
-  beforeEach((done) => {
-    Meteor.call('test.resetDatabase', done);
+  let business;
+  
+  before(() => {
+    business = Factory.create('business');
   });
 
-  it('should have an alphanumeric name of at most 60 characters', () => {
-    assert.isNotEmpty(biz.name);
-    assert.match(biz.name, /^\w+$/); // this regex actually captures empty string too
-    assert.isAtMost(biz.name.length, 60);
+  it('should have a name of at most 60 characters', () => {
+    expect( business.name ).to.not.be.empty;
+    expect( business.desc ).to.have.lengthOf.at.most(60);
   });
 
   it('should have an alphanumeric description of at most 140 characters', () => {
-    assert.isNotEmpty(biz.desc);
-    assert.match(biz.desc, /^.+$/);
-    assert.isAtMost(biz.desc.length, 140);
+    expect( business.name ).to.not.be.empty;
+    expect( business.desc ).to.match(/^.+$/);
+    expect( business.desc ).to.have.lengthOf.at.most(140);
   });
 
   it('should render', () => {
-    const item = shallow(<Business name={biz.name} desc={biz.desc} />);
+    const item = Enzyme.shallow(<Business name={business.name} desc={business.desc} />);
 
-    assert(item.contains(<h2>{biz.name}</h2>), 'the business name');
-    assert(item.contains(<p>{biz.desc}</p>), 'the description');
+    expect( item.find(Card) ).to.have.lengthOf(1);
+    expect( item.find(CardTitle).dive().text() ).to.equal(business.name);
+    expect( item.find(CardText).dive().text() ).to.equal(business.desc);
   });
 
 });
