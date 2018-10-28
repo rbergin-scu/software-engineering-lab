c982783b36db82413b49d9e290b35e04df9dd4f5
Conner Davis <connergdavis@gmail.com>
2018-10-27 18:52:00 -0700

    cleaned up code style in api/submissions

--- imports/api/submissions/submissions.tests.js
+++ imports/api/submissions/submissions.tests.js
@@ -3,36 +3,41 @@ import chai from 'chai';
 import Enzyme from 'enzyme';
 import faker from 'faker';
 import React from 'react';
-//import { Card, CardImg, CardBody, CardTitle, CardText, CardFooter, Button } from 'reactstrap';
 import { Factory } from 'meteor/dburles:factory';
 
+import Businesses from '/imports/api/businesses/businesses';
 import Submissions from './submissions';
-
-//import Submissions from '/imports/ui/components/Submissions';
 
 // configure React test adapter
 Enzyme.configure({adapter: new Adapter()});
-/*
-Meteor.methods({
-  // clear stub data
-  'test.restoreDatabase': () => StubCollections.restore(),
-  
-  // generate stub data
-  'test.stubDatabase': () => StubCollections.stub(['submissions']),
-});
-*/
 
 Factory.define('submission', Submissions, {
   name: () => faker.name.firstName(),
   email: () => faker.internet.email(),
   phoneNumber: () => faker.phone.phoneNumber(),
   gradYear: () => faker.random.number(),
-  businessID: () => faker.random.number(),
+});
+
+Factory.define('business', Businesses, {
+  name: () => faker.company.companyName(),
+  desc: () => faker.company.catchPhrase(),
+  photo: () => faker.image.imageUrl(),
+  type: () => faker.commerce.product(),
+  country: () => faker.address.country(),
+  streetAddress: () => faker.address.streetAddress(),
+  state: () => faker.address.state(),
+  city: () => faker.address.city(),
+  zip: () => faker.address.zipCode(undefined),
+  phoneNumber: () => faker.phone.phoneNumber(),
+  website: () => faker.internet.url(),
+  verified: false,
 });
 
 let expect = chai.expect;
 
 describe('Submissions', () => {
+  
+  let business;
   
   let submission;
   let submission2;
@@ -40,6 +45,7 @@ describe('Submissions', () => {
   let id2;
   
   before(() => {
+    business = Factory.create('business');
     submission = Factory.create('submission');
     submission2 = Factory.create('submission');
     id = submission._id;
@@ -56,28 +62,19 @@ describe('Submissions', () => {
     expect(submission.email).to.have.lengthOf.at.most(60);
   });
   
-  //no Submissions front end yet
-  /*it('should render', () => {
-    const item = Enzyme.shallow(<Submission name={submission.name} />);
-
-    expect( item.find(Card) ).to.have.lengthOf(1);
-    expect( item.find(CardTitle).dive().text() ).to.equal(submission.name);
-  });*/
-  
   it('should be added to the collection', () => {
     Meteor.call('submissions.insert', {
       name: faker.name.firstName(),
       email: faker.internet.email(),
       phoneNumber: faker.phone.phoneNumber(),
       gradYear: faker.random.number(),
-      businessID: faker.random.number(),
+      business: business,
     });
   });
   
   it('should be added to the collection without the optional fields', () => {
     Meteor.call('submissions.insert', {
       name: faker.name.firstName(),
-      businessID: faker.random.number(),
     });
   });
   
@@ -86,24 +83,22 @@ describe('Submissions', () => {
       name: faker.name.firstName(),
       email: faker.internet.email(),
       phoneNumber: faker.phone.phoneNumber(),
-      businessID: faker.random.number(),
     });
   });
   
   it('should be removed from the collection', () => {
     Meteor.call('submissions.remove', {
-      submissionID: id,
+      _id: id,
     });
   });
   
   it('should update the collection', () => {
     Meteor.call('submissions.update', {
+      _id: id,
       name: 'Nani',
       email: submission2.email,
       phoneNumber: submission2.phoneNumber,
       gradYear: submission2.gradYear,
-      businessID: submission2.businessID,
-      submissionID: id2,
     });
   });
   
