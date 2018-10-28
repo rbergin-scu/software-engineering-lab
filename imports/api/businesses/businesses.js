5d34230c525b54ffdb5356579d73946a7fda0f02
Conner Davis <connergdavis@gmail.com>
2018-10-27 18:31:47 -0700

    cleaned up code style in api/businesses

--- imports/api/businesses/businesses.js
+++ imports/api/businesses/businesses.js
@@ -2,49 +2,88 @@ import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
 import SimpleSchema from 'simpl-schema';
 import { Tracker } from 'meteor/tracker';
-import Submissions from "../submissions/submissions";
 
 // create business table
 const Businesses = new Mongo.Collection('businesses');
 
 // establish schema defining each business entry
 Businesses.schema = new SimpleSchema({
-  
   /* title of business */
-  name: { type: String, min: 1, max: 70 },
-
-  /* brief description */
-  desc: { type: String, min: 1, max: 140 },
-
-  /* header photo */
-  photo: { type: String },
-
-  /* country (location) */
-  country: { type: String, min: 4 },
-
-  /* address of business */
-  streetAddress: { type: String, min: 1 },
-
-  /* state (location) */
-  state: { type: String, optional: true, min: 2 },
-
-  /* city (location) */
-  city: { type: String, min: 1 },
-
-  /* zip (location) */
-  zip: { type: String, optional: true, regEx: SimpleSchema.RegEx.ZipCode },
-
-  /* phone number of business */
-  phoneNumber: { type: String, optional: true, regEx: SimpleSchema.RegEx.Phone },
-
-  /* url of website for business */
-  website: { type: String, optional: true, regEx: SimpleSchema.RegEx.Url },
-
-  /* type/category of business (food, entertainment, etc) */
-  type: { type: String, min: 4 },
-
-  verified: { type: Boolean, defaultValue: false },
+  name: {
+    type: String,
+    min: 1,
+    max: 70,
+  },
   
+  /* brief description */
+  desc: {
+    type: String,
+    min: 1,
+    max: 140,
+  },
+  
+  /* header photo */
+  photo: {
+    type: String,
+  },
+  
+  /* type/category of business (food, entertainment, etc) */
+  type: {
+    type: String,
+    min: 4,
+  },
+  
+  /* whether this business has been verified by admin (is public) */
+  verified: {
+    type: Boolean,
+    defaultValue: false,
+  },
+  
+  /* country (location) */
+  country: {
+    type: String,
+    min: 4,
+  },
+  
+  /* address of business */
+  streetAddress: {
+    type: String,
+    min: 1,
+  },
+  
+  /* state (location) */
+  state: {
+    type: String,
+    optional: true,
+    min: 2,
+  },
+  
+  /* city (location) */
+  city: {
+    type: String,
+    min: 1,
+  },
+  
+  /* zip (location) */
+  zip: {
+    type: String,
+    optional: true,
+    regEx: SimpleSchema.RegEx.ZipCode,
+  },
+  
+  /* phone number of business */
+  phoneNumber: {
+    type: String,
+    optional: true,
+    regEx: SimpleSchema.RegEx.Phone,
+  },
+  
+  /* url of website for business */
+  website: {
+    type: String,
+    optional: true,
+    regEx: SimpleSchema.RegEx.Url,
+  },
 }, { tracker: Tracker });
 
 // publish Business data to client
@@ -85,36 +124,40 @@ Meteor.methods({
       });
     }
   },
-
-    'businesses.remove'({ businessID }) {
-        Businesses.remove({
-            _id: businessID
-        });
-    },
-
-    'businesses.update'({
-    name, desc, photo, country, streetAddress, state, city, zip, phoneNumber, website, type, verified, businessID
-    }) {
-        // validate update
-        Businesses.schema.validate({
-            name, desc, photo, country, streetAddress, state, city, zip, phoneNumber, website, type, verified,
-        });
-        // submit to database
-        Businesses.update({_id: businessID }, {
-            name: name,
-            desc: desc,
-            photo: photo,
-            country: country,
-            streetAddress: streetAddress,
-            state: state,
-            city: city,
-            zip: zip,
-            phoneNumber: phoneNumber,
-            website: website,
-            type: type,
-            verified: verified,
-        });
-    },
+  
+  'businesses.remove'({
+    id
+  }) {
+    Businesses.remove({
+      _id: id,
+    });
+  },
+  
+  'businesses.update'({
+    id, name, desc, photo, country, streetAddress, state, city, zip, phoneNumber, website, type, verified,
+  }) {
+    // validate update
+    Businesses.schema.validate({
+      name, desc, photo, country, streetAddress, state, city, zip, phoneNumber, website, type, verified,
+    });
+    
+    // submit to database
+    // TODO need to validate that Business with this id exists!
+    Businesses.update({ _id: id, }, {
+      name: name,
+      desc: desc,
+      photo: photo,
+      country: country,
+      streetAddress: streetAddress,
+      state: state,
+      city: city,
+      zip: zip,
+      phoneNumber: phoneNumber,
+      website: website,
+      type: type,
+      verified: verified,
+    });
+  },
 });
 
 export default Businesses;
--- imports/api/businesses/businesses.tests.js
+++ imports/api/businesses/businesses.tests.js
@@ -1,24 +1,16 @@
 import Adapter from 'enzyme-adapter-react-16';
 import chai from 'chai';
 import Enzyme from 'enzyme';
+import { Factory } from 'meteor/dburles:factory';
 import faker from 'faker';
 import React from 'react';
 import { Card, CardImg, CardBody, CardTitle, CardText, CardFooter, Button } from 'reactstrap';
-import { Factory } from 'meteor/dburles:factory';
 
 import Businesses from './businesses';
 import Business from '/imports/ui/components/Business';
 
 // configure React test adapter
-Enzyme.configure({ adapter: new Adapter() });
-
-Meteor.methods({
-  /* clear stub data */
-  'test.restoreDatabase': () => StubCollections.restore(),
-  
-  /* generate stub data */
-  'test.stubDatabase': () => StubCollections.stub(['businesses']),
-});
+Enzyme.configure({adapter: new Adapter()});
 
 Factory.define('business', Businesses, {
   name: () => faker.company.companyName(),
@@ -38,7 +30,7 @@ Factory.define('business', Businesses, {
 let expect = chai.expect;
 
 describe('Businesses', () => {
-
+  
   let business;
   let business2;
   let id;
@@ -50,94 +42,95 @@ describe('Businesses', () => {
     id = business._id;
     id2 = business2._id;
   });
-
+  
   it('should have a name of at most 60 characters', () => {
-    expect( business.name ).to.not.be.empty;
-    expect( business.name ).to.have.lengthOf.at.most(60);
+    expect(business.name).to.not.be.empty;
+    expect(business.name).to.have.lengthOf.at.most(60);
   });
-
+  
   it('should have an alphanumeric description of at most 140 characters', () => {
-    expect( business.desc ).to.not.be.empty;
-    expect( business.desc ).to.match(/^.+$/);
-    expect( business.desc ).to.have.lengthOf.at.most(140);
+    expect(business.desc).to.not.be.empty;
+    expect(business.desc).to.match(/^.+$/);
+    expect(business.desc).to.have.lengthOf.at.most(140);
   });
-
-    it('should have a country of at most 60 characters', () => {
-        expect( business.country ).to.not.be.empty;
-        expect( business.country ).to.have.lengthOf.at.most(60);
-    });
-
-    it('should have a streetAddress of at most 60 characters', () => {
-        expect( business.streetAddress ).to.not.be.empty;
-        expect( business.streetAddress ).to.have.lengthOf.at.most(60);
-    });
-
-    it('should have a state of at most 30 characters', () => {
-        expect( business.state ).to.not.be.empty;
-        expect( business.state ).to.have.lengthOf.at.most(30);
-    });
-
-    it('should have a phone number of at most 30 characters', () => {
-        expect( business.phoneNumber ).to.not.be.empty;
-        expect( business.phoneNumber ).to.have.lengthOf.at.most(30);
-    });
-
-    it('should have a webite url of at most 100 characters', () => {
-        expect( business.website ).to.not.be.empty;
-        expect( business.website ).to.have.lengthOf.at.most(100);
-    });
-
-    it('should have a type of at most 30 characters', () => {
-        expect( business.type ).to.not.be.empty;
-        expect( business.type ).to.have.lengthOf.at.most(30);
-    });
-
+  
+  it('should have a country of at most 60 characters', () => {
+    expect(business.country).to.not.be.empty;
+    expect(business.country).to.have.lengthOf.at.most(60);
+  });
+  
+  it('should have a streetAddress of at most 60 characters', () => {
+    expect(business.streetAddress).to.not.be.empty;
+    expect(business.streetAddress).to.have.lengthOf.at.most(60);
+  });
+  
+  it('should have a state of at most 30 characters', () => {
+    expect(business.state).to.not.be.empty;
+    expect(business.state).to.have.lengthOf.at.most(30);
+  });
+  
+  it('should have a phone number of at most 30 characters', () => {
+    expect(business.phoneNumber).to.not.be.empty;
+    expect(business.phoneNumber).to.have.lengthOf.at.most(30);
+  });
+  
+  it('should have a webite url of at most 100 characters', () => {
+    expect(business.website).to.not.be.empty;
+    expect(business.website).to.have.lengthOf.at.most(100);
+  });
+  
+  it('should have a type of at most 30 characters', () => {
+    expect(business.type).to.not.be.empty;
+    expect(business.type).to.have.lengthOf.at.most(30);
+  });
+  
   it('should render', () => {
-    const item = Enzyme.shallow(<Business name={business.name} desc={business.desc} />);
-
-    expect( item.find(Card) ).to.have.lengthOf(1);
-    expect( item.find(CardTitle).dive().text() ).to.equal(business.name);
-    expect( item.find(CardText).dive().text() ).to.equal(business.desc);
+    const item = Enzyme.shallow(<Business name={business.name} desc={business.desc}/>);
+    
+    expect(item.find(Card)).to.have.lengthOf(1);
+    expect(item.find(CardTitle).dive().text()).to.equal(business.name);
+    expect(item.find(CardText).dive().text()).to.equal(business.desc);
   });
-
-    it('should be added to the collection', () => {
-        Meteor.call('businesses.insert', {
-            name: faker.company.companyName(),
-            desc: faker.company.catchPhrase(),
-            photo: faker.image.imageUrl(),
-            country: faker.address.country(),
-            streetAddress: faker.address.streetAddress(),
-            state: faker.address.state(),
-            city: faker.address.city(),
-            zip: faker.address.zipCode(undefined),
-            phoneNumber: faker.phone.phoneNumber(),
-            website: faker.internet.url(),
-            type: faker.commerce.product(),
-            verified: false,
-        });
+  
+  it('should be added to the collection', () => {
+    Meteor.call('businesses.insert', {
+      name: faker.company.companyName(),
+      desc: faker.company.catchPhrase(),
+      photo: faker.image.imageUrl(),
+      country: faker.address.country(),
+      streetAddress: faker.address.streetAddress(),
+      state: faker.address.state(),
+      city: faker.address.city(),
+      zip: faker.address.zipCode(undefined),
+      phoneNumber: faker.phone.phoneNumber(),
+      website: faker.internet.url(),
+      type: faker.commerce.product(),
+      verified: false,
     });
-
-    it('should be removed from the collection', () => {
-        Meteor.call('businesses.remove', {
-            businessID: id,
-        });
+  });
+  
+  it('should be removed from the collection', () => {
+    Meteor.call('businesses.remove', {
+      _id: id,
     });
-
-    it('should update the collection', () => {
-        Meteor.call('businesses.update', {
-            name: "NANI!?!?",
-            desc: business2.desc,
-            photo: business2.photo,
-            country: business2.country,
-            streetAddress: business2.streetAddress,
-            state: business2.state,
-            city: business2.city,
-            zip: business2.zip,
-            phoneNumber: business2.phoneNumber,
-            website: business2.website,
-            type: business2.type,
-            verified: business2.verified,
-            businessID: id2,
-        });
+  });
+  
+  it('should update the collection', () => {
+    Meteor.call('businesses.update', {
+      _id: id2,
+      name: 'NANI!?!?',
+      desc: business2.desc,
+      photo: business2.photo,
+      country: business2.country,
+      streetAddress: business2.streetAddress,
+      state: business2.state,
+      city: business2.city,
+      zip: business2.zip,
+      phoneNumber: business2.phoneNumber,
+      website: business2.website,
+      type: business2.type,
+      verified: business2.verified,
     });
+  });
+  
 });
